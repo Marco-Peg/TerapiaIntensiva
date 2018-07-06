766fb1c3056434d0c7f82b591e28bd23e8e3a2d6
Marco-Peg <39377077+Marco-Peg@users.noreply.github.com>
2018-07-06 16:29:23 +0200

    Add files via upload

--- src/Infermiere.java
+++ src/Infermiere.java
@@ -1,15 +1,22 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Insets;
 
 import javax.swing.*;
 
+/**
+ * Sessione per gli infermieri
+ * @author Marco
+ *
+ */
 public class Infermiere extends Personale{
 	
-	
+	/**
+	 * Costruttore: genera finestra della sessione
+	 * @param id identificatore utente
+	 * @param ruolo ruolo
+	 */
 	public Infermiere(String id, String ruolo) {
 		super.id=id;
 		this.ruolo=ruolo;
@@ -18,26 +25,31 @@ public class Infermiere extends Personale{
 
 
 	@Override
+	/**
+	 * Aggiunge le opzioni caratteristiche per infermieri
+	 */
 	JPanel setPanel() {
 		JPanel out=new JPanel();
 		out.setLayout(new BoxLayout(out, BoxLayout.Y_AXIS));
 		//Inserimento nuovo paziente
 		 JButton newPaziente= new JButton("Registra nuovo paziente");
 		 newPaziente.setAlignmentX(Component.CENTER_ALIGNMENT);
-	    newPaziente.setMargin(new Insets(10, 25, 10, 25));
 		newPaziente.addActionListener(new PazienteListener());
 		newPaziente.setMargin(new Insets(10, 25, 10, 25));
 		out.add(newPaziente);
-		//Inserimento prescrizione
-		 JButton prescrizione= new JButton("Registra prescizione");
-		 prescrizione.setAlignmentX(Component.CENTER_ALIGNMENT);
-		prescrizione.addActionListener(new Prescizione());
-		prescrizione.setMargin(new Insets(10, 25, 10, 25));
-		out.add(prescrizione);
+		//Inserimento somministrazione
+		 JButton somministrazione= new JButton("Registra somministrazione");
+		 somministrazione.setAlignmentX(Component.CENTER_ALIGNMENT);
+		somministrazione.addActionListener(new SomministrazioneListener());
+		somministrazione.setMargin(new Insets(10, 25, 10, 25));
+		out.add(somministrazione);
 		return out;
 	}
 
 	@Override
+	/**
+	 * Aggiunge menu caratteristici per infermieri
+	 */
 	JMenu addMenu() {
 		// TODO Auto-generated method stub
 		return new JMenu("");
@@ -45,6 +57,9 @@ public class Infermiere extends Personale{
 
 
 	@Override
+	/**
+	 * Crea l'intestazione per l'infermiere
+	 */
 	JPanel setHeaderPanel() {
 		JPanel out = new JPanel();
 		out.setBorder(BorderFactory.createLineBorder(Color.green));
