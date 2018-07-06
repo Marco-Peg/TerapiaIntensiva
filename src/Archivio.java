ab7671f0986ada7c4896521a51f3ba633ea57477
Righets <39537571+Righets@users.noreply.github.com>
2018-07-06 16:45:52 +0200

    Update Archivio.java

--- src/Archivio.java
+++ src/Archivio.java
@@ -85,7 +85,7 @@ public class Archivio implements Visualizzazione{
 	
 	
 	/**
-	 * Determina se è al completo
+	 * Determina se Ã¨ al completo
 	 */
 	public boolean isFull() {
 		return (dim==listaPazienti.size());
@@ -118,20 +118,71 @@ public class Archivio implements Visualizzazione{
 	}
 
 	public void visualSomm(Paziente id) {
-		// TODO Auto-generated method stub
-		JFrame frm = new JFrame("Somministrazioni ricevute");
-		frm.setSize(400,100);
+		
+		JFrame frm = new JFrame("Somministrazioni");
+		frm.setSize(400,400);
 		frm.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frm.setVisible(true);
 		
-		Container frmCP = frm.getContentPane();
-		
+		JLabel label = new JLabel("SOMMINISTRAZIONI RICEVUTE:");
+
 		JTextArea area = new JTextArea();
+		area.setColumns(20);
+		area.setLineWrap(true);
+		area.setRows(5);
+		area.setWrapStyleWord(true);
+		area.setEditable(false);
+
 		
-		
-		
-		//area.setEditable(false);
-		
+		JScrollPane scroll = new JScrollPane(area);
+
+		GroupLayout layout = new GroupLayout(frm.getContentPane());
+		frm.getContentPane().setLayout(layout);
+        
+        //Create a parallel group for the horizontal axis
+		ParallelGroup hGroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
+        //Create a sequential and a parallel groups
+		SequentialGroup h1 = layout.createSequentialGroup();
+		ParallelGroup h2 = layout.createParallelGroup(GroupLayout.Alignment.TRAILING);
+        //Add a scroll panel and a label to the parallel group h2
+		h2.addComponent(scroll, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE);
+		h2.addComponent(label, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE);
+        
+        //Add a container gap to the sequential group h1
+		h1.addContainerGap();
+        // Add the group h2 to the group h1
+		h1.addGroup(h2);
+		h1.addContainerGap();
+        //Add the group h1 to hGroup
+		hGroup.addGroup(Alignment.TRAILING,h1);
+        //Create the horizontal group
+		layout.setHorizontalGroup(hGroup);
+        
+        //Create a parallel group for the vertical axis
+		ParallelGroup vGroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
+        //Create a sequential group
+		SequentialGroup v1 = layout.createSequentialGroup();
+        //Add a container gap to the sequential group v1
+		v1.addContainerGap();
+        //Add a label to the sequential group v1
+		v1.addComponent(label);
+		v1.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED);
+        //Add scroll panel to the sequential group v1
+		v1.addComponent(scroll, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE);
+		v1.addContainerGap();
+        //Add the group v1 to vGroup
+		vGroup.addGroup(v1);
+        //Create the vertical group
+		layout.setVerticalGroup(vGroup);
+		frm.pack();
+
+		try(FileReader file = new FileReader("Somministrazioni.txt")){
+	        BufferedReader reader =  new BufferedReader(file);
+	        area.read(reader, null);
+		}catch (IOException e) {
+		// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
 		
 	}
 
