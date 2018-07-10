529e179462a36eee143f3077bdbc1b1254b940f0
Righets <39537571+Righets@users.noreply.github.com>
2018-07-10 16:34:35 +0200

    Update Primario.java

--- src/Primario.java
+++ src/Primario.java
@@ -1,4 +1,29 @@
+import java.awt.Component;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class Primario extends Medico{
+
+	JPanel setPanel() {
+		JPanel out=new JPanel();
+		
+		out.add(super.setPanel());
+		
+		JButton report= new JButton("Visualizza Report");
+		report.setAlignmentX(Component.CENTER_ALIGNMENT);
+//		report.addActionListener(new Report());
+		report.setMargin(new Insets(10, 25, 10, 25));
+		out.add(report);
+		
+		JButton chiusura= new JButton("Termine Ricovero");
+		chiusura.setAlignmentX(Component.CENTER_ALIGNMENT);
+		chiusura.addActionListener(new Chiusura());
+		chiusura.setMargin(new Insets(10, 25, 10, 25));
+		out.add(chiusura);
+		
+		return out;
+	}
 	
 }
